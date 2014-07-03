605c44be2c3a4d87fe68169dd03d6ad52222a72f
tomasgreen <tomas.green@gmail.com>
2014-07-03 23:52:33 +0200

    New page transition
    Changed the page transition from fade to slide.

--- js/blog.js
+++ js/blog.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
 	if (('ontouchstart' in document.documentElement)) {
 		document.documentElement.className += ' touch';
 	}
+	var busy = false;
 	var numerOfPages = $('section').length;
 	var currentPage = parseInt(window.location.hash.substr(1)) || numerOfPages;
 	if (currentPage > numerOfPages) {
@@ -28,6 +29,10 @@ $(document).ready(function () {
 		else $('.down').show();
 	};
 	var resetSize = function () {
+		$('body').css('max-width', $(window).innerWidth() + 'px');
+		$('body').css('max-height', $(window).innerHeight() + 'px');
+		$('body').css('width', $(window).innerWidth() + 'px');
+		$('body').css('height', $(window).innerHeight() + 'px');
 		$('section').width($(window).innerWidth());
 		$('section').height($(window).innerHeight());
 		$('article').centerize();
@@ -39,19 +44,40 @@ $(document).ready(function () {
 		var nextPage = 1;
 		if (direction == 'up') nextPage = currentPage + 1;
 		else if (direction == 'down') nextPage = currentPage - 1;
-		if (nextPage < 1 || nextPage > numerOfPages) return;
+		if (nextPage < 1 || nextPage > numerOfPages || busy) return;
 		busy = true;
 		var $prevPage = $('#' + currentPage);
 		var $nextPage = $('#' + nextPage);
 		currentPage = nextPage;
 		window.location.hash = '#' + currentPage;
 		$nextPage.lazyLoad();
+		/*
 		$prevPage.removeClass('fadeIn').addClass('fadeOut');
 		$nextPage.removeClass('fadeOut').css('display', 'block').addClass('fadeIn');
+		*/
+		if (direction == 'down') {
+			$prevPage.addClass('current moveToTop');
+			$nextPage.addClass('current moveFromBottom');
+		} else {
+			$prevPage.addClass('current moveToBottom');
+			$nextPage.addClass('current moveFromTop');
+		}
+		$nextPage.on('webkitAnimationEnd', function (event) {
+			busy = false;
+			$nextPage.removeClass('moveToTop moveFromBottom moveToBottom moveFromTop');
+			$nextPage.addClass('current');
+			$nextPage.off('webkitAnimationEnd');
+		});
+		$prevPage.on('webkitAnimationEnd', function (event) {
+			busy = false;
+			$prevPage.removeClass('moveToTop moveFromBottom moveToBottom moveFromTop');
+			$prevPage.removeClass('current');
+			$prevPage.off('webkitAnimationEnd');
+		});
 		resetSize();
 		updateChevrons();
 	};
-	$('#' + currentPage).css('display', 'block');
+	$('#' + currentPage).addClass('current');
 	$('#' + currentPage).lazyLoad();
 	resetSize();
 	updateChevrons();
