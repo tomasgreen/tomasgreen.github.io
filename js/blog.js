6640f7a18d4b2a86164fae5e3a5c6502a8f10976
tomasgreen <tomas.green@gmail.com>
2014-07-03 21:35:46 +0200

    Removed scroll
    So the scroll was pretty lame and buggy so I chained it to keybindings
    and links instead.

--- js/blog.js
+++ js/blog.js
@@ -18,6 +18,12 @@ $(document).ready(function () {
 			$obj.attr('data-bg', '');
 		}
 	};
+	var updateChevrons = function () {
+		if (currentPage == numerOfPages) $('.up').hide();
+		else $('.up').show();
+		if (currentPage == 1) $('.down').hide();
+		else $('.down').show();
+	};
 	var resetSize = function () {
 		$('section').width($(window).innerWidth());
 		$('section').height($(window).innerHeight());
@@ -26,12 +32,11 @@ $(document).ready(function () {
 	$('section').each(function (i) {
 		$(this).attr('id', numerOfPages - i);
 	});
-	var busy = false;
 	var changePage = function (direction) {
 		var nextPage = 1;
 		if (direction == 'up') nextPage = currentPage + 1;
 		else if (direction == 'down') nextPage = currentPage - 1;
-		if (nextPage < 1 || nextPage > numerOfPages || busy) return;
+		if (nextPage < 1 || nextPage > numerOfPages) return;
 		busy = true;
 		var $prevPage = $('#' + currentPage);
 		var $nextPage = $('#' + nextPage);
@@ -41,36 +46,28 @@ $(document).ready(function () {
 		$prevPage.removeClass('fadeIn').addClass('fadeOut');
 		$nextPage.removeClass('fadeOut').css('display', 'block').addClass('fadeIn');
 		resetSize();
-		setTimeout(function () {
-			busy = false;
-		}, 1500);
+		updateChevrons();
 	};
 	$('#' + currentPage).css('display', 'block');
 	$('#' + currentPage).lazyLoad();
 	resetSize();
+	updateChevrons();
 	$(window).resize(function () {
 		resetSize();
 	});
-	var ts;
-	$(document).bind('touchstart', function (e) {
-		ts = e.originalEvent.touches[0].clientY;
-		return false;
-	});
-	$(document).bind('touchend', function (e) {
-		var te = e.originalEvent.changedTouches[0].clientY;
-		if (ts > te + 5) {
-			changePage('down');
-		} else if (ts < te - 5) {
+	$(document).keydown(function (e) {
+		if (e.keyCode == 38) {
 			changePage('up');
+			return false;
+		} else if (e.keyCode == 40 || e.keyCode == 32) {
+			changePage('down');
+			return false;
 		}
-		return false;
 	});
-	$(document).bind('DOMMouseScroll', function (e) {
-		changePage((e.originalEvent.detail > 0) ? 'down' : 'up');
-		return false;
+	$('.up').click(function () {
+		changePage('up');
 	});
-	$(document).bind('mousewheel', function (e) {
-		changePage((e.originalEvent.wheelDelta < 0) ? 'down' : 'up');
-		return false;
+	$('.down').click(function () {
+		changePage('down');
 	});
 });
