ef9e3f97f979a9bbcca123ec20bc420d5c66427a
Tomas Green <tomas.green@gmail.com>
2014-07-04 12:52:00 +0200

    The scroll is back
    I’ve reintroduced the scrolling function, new and improved.
    I’ve also updated the lazy image load with a nice fade transition.

--- js/blog.js
+++ js/blog.js
@@ -1,27 +1,41 @@
+$.fn.centerize = function () {
+	$(this).each(function () {
+		var $obj = $(this);
+		$obj.css('margin-top', parseInt(($obj.parent().outerHeight() - $obj.outerHeight()) / 2) + 'px');
+	});
+};
+$.fn.lazyLoad = function () {
+	var $obj = $(this);
+	if ($obj.attr('data-bg')) {
+		var i = new Image();
+		i.onload = function () {
+			$obj.css('background-image', 'url(' + i.src + ')');
+			$obj.attr('data-bg', '');
+		};
+		i.src = $obj.attr('data-bg');
+	}
+};
+$.fn.absoluteSize = function (width, height) {
+	var $obj = $(this);
+	$obj.css('max-width', width + 'px');
+	$obj.css('max-height', height + 'px');
+	$obj.css('width', width + 'px');
+	$obj.css('height', height + 'px');
+	$obj.css('min-width', width + 'px');
+	$obj.css('min-height', height + 'px');
+};
 $(document).ready(function () {
 	if (('ontouchstart' in document.documentElement)) {
 		document.documentElement.className += ' touch';
 	}
-	var busy = false;
+	var nextPageBusy = false;
+	var prevPageBusy = false;
 	var numerOfPages = $('section').length;
 	var currentPage = parseInt(window.location.hash.substr(1)) || numerOfPages;
 	if (currentPage > numerOfPages) {
 		currentPage = numerOfPages;
 		window.location.hash = '#' + currentPage;
 	}
-	$.fn.centerize = function () {
-		$(this).each(function () {
-			var $obj = $(this);
-			$obj.css('margin-top', parseInt(($obj.parent().outerHeight() - $obj.outerHeight()) / 2) + 'px');
-		});
-	};
-	$.fn.lazyLoad = function () {
-		var $obj = $(this);
-		if ($obj.attr('data-bg')) {
-			$obj.css('background-image', 'url(' + $obj.attr('data-bg') + ')');
-			$obj.attr('data-bg', '');
-		}
-	};
 	var updateChevrons = function () {
 		if (currentPage == numerOfPages) $('.up').hide();
 		else $('.up').show();
@@ -29,32 +43,27 @@ $(document).ready(function () {
 		else $('.down').show();
 	};
 	var resetSize = function () {
-		$('body').css('max-width', $(window).innerWidth() + 'px');
-		$('body').css('max-height', $(window).innerHeight() + 'px');
-		$('body').css('width', $(window).innerWidth() + 'px');
-		$('body').css('height', $(window).innerHeight() + 'px');
-		$('section').width($(window).innerWidth());
-		$('section').height($(window).innerHeight());
+		$('body').absoluteSize($(window).innerWidth(), $(window).innerHeight());
+		$('section').absoluteSize($(window).innerWidth(), $(window).innerHeight());
 		$('article').centerize();
+	};
+	var busy = function () {
+		return (prevPageBusy || nextPageBusy);
 	};
 	$('section').each(function (i) {
 		$(this).attr('id', numerOfPages - i);
 	});
 	var changePage = function (direction) {
 		var nextPage = 1;
-		if (direction == 'up') nextPage = currentPage + 1;
-		else if (direction == 'down') nextPage = currentPage - 1;
-		if (nextPage < 1 || nextPage > numerOfPages || busy) return;
-		busy = true;
+		nextPage = (direction == 'up') ? currentPage + 1 : currentPage - 1;
+		if (nextPage < 1 || nextPage > numerOfPages || busy()) return;
 		var $prevPage = $('#' + currentPage);
 		var $nextPage = $('#' + nextPage);
+		nextPageBusy = true;
+		prevPageBusy = true;
 		currentPage = nextPage;
 		window.location.hash = '#' + currentPage;
 		$nextPage.lazyLoad();
-		/*
-		$prevPage.removeClass('fadeIn').addClass('fadeOut');
-		$nextPage.removeClass('fadeOut').css('display', 'block').addClass('fadeIn');
-		*/
 		if (direction == 'down') {
 			$prevPage.addClass('current moveToTop');
 			$nextPage.addClass('current moveFromBottom');
@@ -63,13 +72,13 @@ $(document).ready(function () {
 			$nextPage.addClass('current moveFromTop');
 		}
 		$nextPage.on('webkitAnimationEnd', function (event) {
-			busy = false;
+			nextPageBusy = false;
 			$nextPage.removeClass('moveToTop moveFromBottom moveToBottom moveFromTop');
 			$nextPage.addClass('current');
 			$nextPage.off('webkitAnimationEnd');
 		});
 		$prevPage.on('webkitAnimationEnd', function (event) {
-			busy = false;
+			prevPageBusy = false;
 			$prevPage.removeClass('moveToTop moveFromBottom moveToBottom moveFromTop');
 			$prevPage.removeClass('current');
 			$prevPage.off('webkitAnimationEnd');
@@ -98,5 +107,40 @@ $(document).ready(function () {
 	});
 	$('.down').click(function () {
 		changePage('down');
+	});
+	var ts;
+	var lastScroll = new Date();
+	$(document).bind('touchstart', function (e) {
+		ts = e.originalEvent.touches[0].clientY;
+		return false;
+	});
+	$(document).bind('touchend', function (e) {
+		var te = e.originalEvent.changedTouches[0].clientY;
+		if (ts > te + 5) {
+			changePage('down');
+		} else if (ts < te - 5) {
+			changePage('up');
+		}
+		return false;
+	});
+	$(document).bind('DOMMouseScroll', function (e) {
+		if (new Date() - lastScroll < 40) {
+			lastScroll = new Date();
+			return;
+		}
+		lastScroll = new Date();
+		changePage((e.originalEvent.detail > 0) ? 'down' : 'up');
+		e.preventDefault();
+		return false;
+	});
+	$(document).bind('mousewheel', function (e) {
+		if (new Date() - lastScroll < 40) {
+			lastScroll = new Date();
+			return;
+		}
+		lastScroll = new Date();
+		changePage((e.originalEvent.wheelDelta < 0) ? 'down' : 'up');
+		e.preventDefault();
+		return false;
 	});
 });
